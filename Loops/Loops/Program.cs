f274c14450268a1c91de0e0dbaac434123ea6af5
unknown <Ulvi Abdullazada@b3-0.bs101.local>
2025-03-13 10:55:19 +0400

    Array/

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,45 @@
+﻿
+#region For
+//int num = Convert.ToInt32(Console.ReadLine());
+//int num = 2;
+//for(; true;)
+//{
+//    num++;
+//    Console.WriteLine(num);
+//    if (num == 10)
+//    {
+//        break;
+//    }
+//}
+//for (int i = 0; i < 0; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        continue;
+//    }
+//    Console.WriteLine(i);
+//}
+#endregion
+
+#region While
+//int i = 0;
+//while (i <= 10)
+//{
+//    Console.WriteLine(i++);
+//}
+//int num = Convert.ToInt32(Console.ReadLine());
+//int hasil = 1;
+//while (num > 1)
+//{
+//    hasil *= num;
+//    num--;
+//}
+//Console.WriteLine(hasil);
+
+
+int i = 10;
+do
+{
+    Console.WriteLine(i++);
+} while (i > 10);
+#endregion
