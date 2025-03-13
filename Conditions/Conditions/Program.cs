618dfcf3abd3747e86241388581ad89ddb4d8f0d
unknown <Ulvi Abdullazada@b3-0.bs101.local>
2025-03-13 10:54:47 +0400

    Conditions

---
+++ Conditions/Conditions/Program.cs
@@ -0,0 +1,104 @@
+﻿#region If, else
+//int age = Convert.ToInt32(Console.ReadLine());
+//if(age > 12)
+//{
+//    Console.WriteLine("Filme baxa bilersiniz");
+//}
+//else if(age == 12)
+//{
+//    Console.WriteLine("Hediyyenizi goturun");
+//}
+//else
+//{
+//    Console.WriteLine("Film izlemeye yashiniz chatmir");
+//}
+//int num = 0;
+//if(num > 0)
+//{
+//    Console.WriteLine("Eded musbetdir");
+//}
+//else if(num == 0)
+//{
+//    Console.WriteLine("Eded ne musbet ne de menfidir");
+//}
+//else
+//{
+//    Console.WriteLine("Eded menfidir");
+//}
+#endregion
+#region Switch case
+//int dayOfWeek = 14;
+//switch (dayOfWeek)
+//{
+//    case 1:
+//        Console.WriteLine("Bazar ertesi");
+//        break;
+//    case 2:
+//        Console.WriteLine("Chershenbe axshami");
+//        break;
+//    case 3:
+//        Console.WriteLine("Chershenbe");
+//        break;
+//    case 4:
+//        Console.WriteLine("Cume axshami");
+//        break;
+//    case 5:
+//        Console.WriteLine("Cume");
+//        break;
+//    case 6:
+//        Console.WriteLine("Shenbe");
+//        break;
+//    case 7:
+//        Console.WriteLine("Bazar");
+//        break;
+//    default:
+//        Console.WriteLine("Duzgun deyer daxil edin");
+//        break;
+//}
+//switch (dayOfWeek)
+//{
+//    case 6:
+//        Console.WriteLine("alti");
+//        break;
+//    case > 5:
+//        Console.WriteLine("5den boyukdur");
+//        break;
+//    case > 1:
+//        Console.WriteLine("1den boyukdur");
+//        break;
+//    case -2:
+//        Console.WriteLine("asdasd");
+//        break;
+//}
+//DRY - Dont repeat yourself
+//string month = "Fevral";
+//int year = 2024;
+//switch (month)
+//{
+//    case "Yanvar":
+//    case "Mart":
+//    case "May":
+//    case "Iyul":
+//    case "Avqust":
+//    case "Oktyabr":
+//    case "Dekabr":
+//        Console.WriteLine("31");
+//        break;
+//    case "Fevral":
+//        if (year % 4 == 0)
+//        {
+//            Console.WriteLine("29");
+//        }
+//        else
+//        {
+//            Console.WriteLine("28");
+//        }
+//        break;
+//    case "Aprel":
+//    case "Iyun":
+//    case "Sentyabr":
+//    case "Noyabr":
+//        Console.WriteLine("30");
+//        break;
+//}
+#endregion
