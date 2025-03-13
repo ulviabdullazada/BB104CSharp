c2c9c67b2b90217b12964a36db40c5b401b6d91d
unknown <Ulvi Abdullazada@b3-0.bs101.local>
2025-03-13 10:56:31 +0400

    Array

---
+++ Array/Array/Program.cs
@@ -0,0 +1,36 @@
+﻿//int[] numbers = { 1, 2, 5, 12, 19};
+////int[] numbers2 = [1, 2, 5, 12, 19];
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+//int[] numbers = new int[3];
+//numbers[1] = 22;
+//for (int i = 0; i < numbers.Length; i++)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+//int[,] nums = new int[2, 2];
+//int[,] nums2 = { //[3,4]
+//    {12,1,0,4 },
+//    {0,9,0,4 },
+//    {7,0,16,5 },
+//};
+//Console.WriteLine(nums2[2,2]);
+//for (int i = 0; i < nums2.GetLength(0); i++)
+//{
+//    for (int b = 0; b < nums2.GetLength(1); b++)
+//    {
+//        Console.WriteLine(nums2[2,3]);
+//    }
+//}
+//Console.WriteLine(nums2.GetLength(1));
+int[] nums1 = { 1, 2, 3 };
+int[] nums2 = { 2, 4, 6 };
+int nums3 = 6;
+//int[] result = [..nums1,..nums2, nums3,3,4,5];
+//for (int i = 0; i < result.Length; i++)
+//{
+//    Console.WriteLine(result[i]);
+//}
+Console.WriteLine(nums2[^2]);
